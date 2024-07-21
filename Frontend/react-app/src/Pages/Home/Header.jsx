565c56e849f42995e94a0fe62dc92bea9cb45dde
Enes Furkan Saglam <enesfurkansaglam@gmail.com>
2024-07-21 22:18:35 +0300

    Project created

---
+++ Frontend/react-app/src/Pages/Home/Header.jsx
@@ -0,0 +1,18 @@
+export default function Header(){
+    return (
+
+    <header>
+      <div className="logo"></div>
+      <nav>
+        <ul>
+          <li><a href="#home">Ana Sayfa</a></li>
+          <li><a href="#about">Hakkında</a></li>
+          <li><a href="#services">Hizmetler</a></li>
+          <li><a href="#contact">İletişim</a></li>
+        </ul>
+      </nav>
+    </header>
+
+
+    );
+}
