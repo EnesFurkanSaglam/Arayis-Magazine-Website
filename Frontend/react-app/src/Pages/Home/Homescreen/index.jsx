d38f983cedb18cd75a87e9a681b94099802c03b2
Enes Furkan Saglam <enesfurkansaglam@gmail.com>
2024-07-31 21:24:13 +0300

    The header section and menu design are finished

--- Frontend/react-app/src/Pages/Home/Homescreen/index.jsx
+++ Frontend/react-app/src/Pages/Home/Homescreen/index.jsx
@@ -1,11 +1,9 @@
 import Header from "../Header";
 
-export default function Home(){
-    return(
+export default function Home() {
+    return (
         <>
-        
             <Header />
-
         </>
     )
 }
