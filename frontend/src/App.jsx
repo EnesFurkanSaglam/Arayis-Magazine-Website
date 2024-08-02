91c7b2293132a7f7b997f3bca1c904cb67a8ba56
Enes Furkan Saglam <enesfurkansaglam@gmail.com>
2024-08-02 14:27:46 +0300

    folders organized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import './App.css'
+import Header from './Pages/Home/Header'
+
+
+function App() {
+
+  return (
+
+    <>
+      <Header />
+
+    </>
+
+  )
+}
+
+export default App
